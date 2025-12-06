ee750a015cdbe51e590426145e16f4f5bced71e5
i5ayahcp <boonpia509@gmail.com>
2025-12-06 16:50:33 +0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-import { pvdata } from "./data.js";
+// import {data} from "./data.js"
+
+
+
+// let container = document.getElementById('xxx')
+
+
+// data.forEach(i => {
+//   console.log(i.description)
+  
+//   let div = document.createElement('div');
+//   div.className = "info_div"
+//   let item2 = document.createElement('div');
+//   item2.className = "item2"
+//   let item = document.createElement('div');
+//   item.className = "info"
+//   let p = document.createElement('p');
+//   p.className = "text"
+//   p.innerHTML = i.name
+//   item.innerHTML = i.description
+//   console.log(item)
+//   item.appendChild(item2)
+//   div.appendChild(item)
+//   console.log(div)
+//   container.appendChild(div)
+
+// })
+
